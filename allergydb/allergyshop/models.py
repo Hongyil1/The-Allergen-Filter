2c526e304c3b097685fa5cb2fcc11d3833839be3
Daniel Hocking <daniel.hocking@gmail.com>
2018-08-05 09:25:37 +1000

    add extra details to product tiles

--- allergydb/allergyshop/models.py
+++ allergydb/allergyshop/models.py
@@ -51,6 +51,16 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    def get_stores(self):
+        if self.stores:
+            return self.stores.split(' ')
+        return []
+
+    def get_allergens(self):
+        if self.allergens:
+            return self.allergens.split(' ')
+        return []
+
     class Meta:
         ordering = ('name',)
 
