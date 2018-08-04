0ef18a8ead3427618e84c6d2bb2ffe662d4704d6
Hongyil1 <hylxmu@163.com>
2018-08-05 02:32:58 +1000

    ingred

--- allergydb/allergyshop/models.py
+++ allergydb/allergyshop/models.py
@@ -41,10 +41,13 @@ class Barcode(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=250, db_index=True)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    allergens = models.ManyToManyField(Allergen)
-    stores = models.ManyToManyField(Store)
+    # allergens = models.ManyToManyField(Allergen)
+    allergens = models.CharField(max_length=250, db_index=True, default=None)
+    stores = models.CharField(max_length=250, db_index=True, default=None)
     barcode = models.CharField(max_length=250, db_index=True, default=None)
     picture = models.CharField(max_length=250, db_index=True, default=None)
+    ingredients = models.TextField()
+
     def __str__(self):
         return self.name
 
--- allergydb/allergyshop/views.py
+++ allergydb/allergyshop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Allergen, Store, Product 
 from django.shortcuts import render_to_response
+from django.db.models import Q
 
 
 def listproducts(request, category_slug=None):
@@ -17,6 +18,38 @@ def product_detail(request, id):
 # Create your views here.
 
 def index(request):
-    context = {}
+
     products = Product.objects.all()
-    return render_to_response('allergyshop/index.html', {'context': context, 'products': products})
+
+    results = ""
+
+    if 'barcode_search' in request.GET:
+        search = request.GET.get('barcode_search')
+        if search != "":
+            results = Product.objects.filter(barcode__icontains=search)
+        else:
+            results = ""
+
+    if 'item_search' in request.GET:  # If the form is submitted
+        item_name = request.GET.get('item_search')
+        item_category = request.GET.get('category')
+        item_allergens = request.GET.get('allergens')
+        print("name: ", item_name, " category: ", item_category, "allergens: ", item_allergens)
+        allergen_list = item_allergens.split(",")
+
+        products_not_allergen = Product.objects.all()
+        for allergen in allergen_list:
+            products_not_allergen = products_not_allergen.filter(~Q(allergens__icontains=allergen))
+            # print("aaaa")
+            # print(len(products_not_allergen))
+
+        if item_category != "":
+            if len(products_not_allergen) != 0:
+                # print("ccc")
+                results = products_not_allergen.filter(category__icontains=item_category)
+                print("results: ", results)
+                if len(results) != 0:
+                    # print("ddd")
+                    results = results.filter(name__icontains=item_name)
+
+    return render_to_response('allergyshop/index.html', {'results': results, 'products': products})
